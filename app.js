cd1799c06100c1c8a7aceed4bcf42e92333fbeeb
Kautja <126161294+Kautja@users.noreply.github.com>
2023-05-26 23:07:03 +0200

    Added files

---
+++ app.js
@@ -0,0 +1,71 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+//TOGGLE
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
+
+// Get the form element
+const feedbackForm = document.getElementById('feedbackForm');
+
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get the input values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // Validate the input values (you can add more validation logic here)
+  if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+    alert('Please fill in all the fields.');
+    return;
+  }
+
+  // Create the response message
+  const responseMessage = `
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Message:</strong> ${message}</p>
+  `;
+
+  // Display the response message
+  const responseContainer = document.getElementById('response');
+  responseContainer.innerHTML = responseMessage;
+
+  // Clear the form
+  feedbackForm.reset();
+}
+
+// Add event listener to the form submission
+feedbackForm.addEventListener('submit', handleSubmit);
